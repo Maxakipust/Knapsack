9b92a70d849868c718d6f7e24df3bdc9dd4bc540
maxkipust <maxakipust@gmail.com>
2019-11-10 17:00:49 -0500

    added a solution to find the number of shortest paths between two nodes

--- src/NumberOfShortestPaths/Node.java
+++ src/NumberOfShortestPaths/Node.java
@@ -5,10 +5,64 @@ import java.util.List;
 
 public class Node {
     List<Edge> edges = new ArrayList<>();
+    String name;
+    boolean visited = false;
+    int val = Integer.MAX_VALUE;
+    List<Node> lowNodes = new ArrayList<>();
 
-    public Node(){
+    /**
+     * gets a list of unvisited children
+     * @return a list of nodes
+     */
+    public List<Node> getUnvistedChildren(){
+        List<Node> res = new ArrayList<>();
+        for(Edge e: edges){
+            if(!e.dest.visited){
+                res.add(e.dest);
+            }
+        }
+        return res;
     }
 
+    public Node(String name){
+        this.name = name;
+    }
+
+    /**
+     * dfs to get the sum of paths to the start
+     * @param start the start node
+     * @return the number of paths from start to this node
+     */
+    public int countShortest(Node start){
+        if(this == start){
+            return 1;
+        }
+        int sum = 0;
+        for(Node n: lowNodes){
+            sum += n.countShortest(start);
+        }
+        return sum;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name: ").append(this.name).append("\n");
+        sb.append("Visited: ").append(this.visited).append("\n");
+        sb.append("Val: ").append(this.val).append("\n");
+        sb.append("lowNodes: {");
+        for(Node n: lowNodes){
+            sb.append(n.name).append(",");
+        }
+        sb.append("}");
+
+        return sb.toString();
+    }
+
+    /**
+     * add an edge to this node
+     * @param to the node to connect it to
+     * @param weight the weight of the edge
+     */
     public void addEdge(Node to, int weight){
         Edge e = new Edge(this, to , weight);
         edges.add(e);
--- src/NumberOfShortestPaths/NumberOfShort.java
+++ src/NumberOfShortestPaths/NumberOfShort.java
@@ -1,18 +1,57 @@
 package NumberOfShortestPaths;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class NumberOfShort {
 
-    int numberOfSortest( Node start, Node end){
-        
+    /**
+     * calculates the number of shortest paths from start to end
+     * @param start the starting node
+     * @param end the ending node
+     * @return the number of paths
+     */
+    static int numberOfSortest( Node start, Node end){
+        //start off the bfs from start
+        List<Node> queue = new LinkedList<>();
+        queue.add(start);
+        start.val = 0;
+
+        while(queue.size() != 0){
+            Node n = queue.get(0);
+            for(Edge e: n.edges){
+                int newVal = n.val + e.weight;
+                //if we have a path with the same value as it already has, then we just add it
+                if(e.dest.val == newVal){
+                    e.dest.lowNodes.add(n);
+                }else if(e.dest.val > newVal){
+                    // if we have a lower path we need to reset it
+                    e.dest.lowNodes = new ArrayList<>();
+                    e.dest.lowNodes.add(n);
+                    e.dest.val = newVal;
+                }
+            }
+            queue.addAll(n.getUnvistedChildren());
+            n.visited = true;
+            queue.remove(0);
+        }
+        //then we need to do a dfs from the end to the start counting the paths to start
+        return end.countShortest(start);
     }
 
     public static void main(String[] args) {
-        int graph [][] = new int[][] {
-                {0,3,2},//0
-                {3,0,5},//1
-                {2,5,0} //2
-               //0 1 2
-        };
-        System.out.println(numberOfSortest(graph, 1, 2)); //this should be 2
+        Node n1 = new Node("n1");
+        Node n2 = new Node("n2");
+        Node n3 = new Node("n3");
+        Node n4 = new Node("n4");
+        n1.addEdge(n2, 3);
+        n1.addEdge(n3, 2);
+        n1.addEdge(n4, 5);
+        n2.addEdge(n4, 2);
+        n3.addEdge(n2, 1);
+        n3.addEdge(n4, 3);
+        System.out.println(numberOfSortest(n1, n4));
     }
 }
